860efd8c6d2544f4d98cf7c1d0a5b41baa52bb9d
srneres101 <sr.neres101@gmail.com>
2026-06-30 18:21:48 -0300

    Adição da primeira verção do README, adição dos primeiros arquivos em python e Java

---
+++ 1000-1037 Java/HelloWorld.java
@@ -0,0 +1,14 @@
+/*Atividade 1000
+  Problem 1000*/
+
+import java.io.IOException;
+
+//Troque a classe por Main para aplicar no Beecrowd
+//Change the class name to Main to submit to Beecrowd
+public class HelloWorld {
+ 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+    
+}
